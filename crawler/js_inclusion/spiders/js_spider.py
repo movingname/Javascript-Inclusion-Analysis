da5f938a6e28fed5bec0a790c9e9be87341c7282
Mingyi <rvlfly@gmail.com>
2016-08-27 22:51:58 -0400

    Scrapy now works with selenium

--- crawler/js_inclusion/spiders/js_spider.py
+++ crawler/js_inclusion/spiders/js_spider.py
@@ -1,28 +1,63 @@
 import scrapy
+from selenium import webdriver
+
+# We rely on scrapy to find and follow links, while we use selenium
+# to extract data.
 
 class JSSpider(scrapy.Spider):
     name = "js_spider"
-    # Don't use *.uber.com.
-    # allowed_domains = ["uber.com"]
-    allowed_domains = ["instacart.com"]
-    start_urls = [
-        #"https://help.uber.com/",
-        #"https://developer.uber.com/"
-        "https://www.instacart.com",
-        "https://partners.instacart.com",
-        "https://shoppers.instacart.com"
-    ]
+
+    def __init__(self, site="instacart"):
+
+        self.site = site
+        self.driver = webdriver.Firefox()
+
+        # Should not use something like *.uber.com.
+        self.allowed_domains = []
+        self.add_lines_from_file("../data/" + site + "_domains.txt", self.allowed_domains)
+
+        self.start_urls = []
+        self.add_lines_from_file("../data/" + site + "_seeds.txt", self.start_urls)
+
+    def add_lines_from_file(self, filename, _list):
+        with open(filename) as f:
+            for line in f:
+                url = line.strip()
+                if url != "":
+                    _list.append(url)
 
     def parse(self, response):
 
-        self.parse_js_links(response)
+        self.get_js_links(response)
+
+        # self.parse_js_links(response)
         for href in response.xpath("//a/@href"):
             url = response.urljoin(href.extract())
-            yield scrapy.Request(url, callback=self.parse_js_links)
+            yield scrapy.Request(url, callback=self.get_js_links)
+            # yield scrapy.Request(url, callback=self.parse_js_links)
 
+    # We use selenium to extract script src
+    # The instacart case show than many script src are dynamically generated.
+    def get_js_links(self, response):
+
+        self.driver.get(response.url)
+
+        elements = self.driver.find_elements_by_xpath('//script')
+
+        with open("../data/" + self.site + "_result.txt", 'a') as f:
+            f.write("Page:" + response.url + "\n")
+            for element in elements:
+                js_src = element.get_attribute("src")
+
+                if js_src != "":
+                    f.write(js_src + "\n")
+                    print(js_src)
+
+    # Get js links by scrapy. However, since scrapy only handles static page,
+    # we could miss some javascript inclusions.
     def parse_js_links(self, response):
-        # with open("data/uber.txt", 'a') as f:
-        with open("data/instacart.txt", 'a') as f:
+        # with open("../data/uber.txt", 'a') as f:
+        with open("../data/instacart.txt", 'a') as f:
             f.write("Page:" + response.url + "\n")
             for sel in response.xpath('//script/@src'):
                 f.write(sel.extract() + "\n")
---
+++ crawler/readme.md
@@ -0,0 +1,8 @@
+
+
+I use Anaconda on a Windows machine. To start the crawler, do the following steps:
+
+1. activate root
+  - root is the name of my Anaconda Python env.
+  
+2. In crawler/, do scrapy crawl js_spider -a site=<site name>
